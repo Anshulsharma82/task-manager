88bff0bbda0a36d27c93cd0dd7c8a887703e1d8f
Anshulsharma82 <anshulsharma4445@gmail.com>
2024-05-28 07:32:11 +0530

    fix-minor-issues

--- app.js
+++ app.js
@@ -6,15 +6,18 @@ require('dotenv').config()
 const unmatchedRoutes = require('./middleware/unmatchedRoutes')
 const errorHandler = require('./middleware/error-handler')
 
+app.use(express.static('./public'));
 app.use(express.json()) // Parse JSON req
+
+
 app.use('/api/v1/tasks', tasks) // Routes
 app.use(unmatchedRoutes) // Middleware: To Handle routes which we have not specified
 app.use(errorHandler) // To handle unknown errors(default errors)
+const PORT = process.env.PORT
 
 const start = async () => {
     try {
         await connectDB(process.env.MONGO_URI)
-        const PORT = process.env.PORT || 3000
         app.listen(PORT, () => {
             console.log(`Server is running on port number ${PORT}.....`)
         })
--- controllers/tasks.js
+++ controllers/tasks.js
@@ -3,63 +3,63 @@ const asyncWrapper = require('../middleware/async-handle-try-catch')
 const { createCustomError } = require('../errors/customError')
 
 const getAllTasks = asyncWrapper(async (req, res) => {
-    const data = await TaskModel.find()
-    res.status(200).json({ success: true, data: data })
+    const tasks = await TaskModel.find()
+    res.status(200).json({ tasks })
 
 })
 
 const createTask = asyncWrapper(async (req, res) => {
-    const result = await TaskModel.create(req.body)
-    res.status(201).json(result)
+    const tasks = await TaskModel.create(req.body)
+    res.status(201).json(tasks)
 
 })
 
 const updateTask = asyncWrapper(async (req, res, next) => {
 
-    const { id } = req.params
-    const task = await TaskModel.findByIdAndUpdate(id, req.body, {
+    const { id: taskID } = req.params
+    const task = await TaskModel.findOneAndUpdate({_id: taskID}, req.body, {
         new: true,
         runValidators: true
     })
     if (!task) {
         return next(createCustomError('no task with the mentioned ID',404))
     }
-    res.status(200).json({ success: true, data: task })
+    res.status(200).json({ task })
 
 
 })
 
 const deleteTask = asyncWrapper(async (req, res, next) => {
 
-    const { id } = req.params
-    const task = await TaskModel.findByIdAndDelete(id)
-    if (!task) {
+    const { id: taskID } = req.params
+    const tasks = await TaskModel.findOneAndDelete({_id: taskID})
+    if (!tasks) {
         return next(createCustomError('no task with the mentioned ID',404))
     }
-    res.status(200).json({ success: true, deletedTask: task })
+    res.status(200).json({  tasks })
 
 })
 
 const getTask = asyncWrapper(async (req, res, next) => {
-    const { id } = req.params
-    const data = await TaskModel.findById(id)
-    if (!data) {
-        return next(createCustomError('no task with the mentioned ID',404))
+    const { id: taskID } = req.params
+    const task = await TaskModel.findOne({ _id: taskID })
+    if (!task) {
+      return next(createCustomError(`No task with id : ${taskID}`, 404))
     }
-    res.status(200).json({ success: true, data: data })
-
-})
+  
+    res.status(200).json({ task })
+  })
 
 const editTask = asyncWrapper(async (req, res, next) => {
-    const { id } = req.params
-    const task = await TaskModel.findByIdAndUpdate(id, req.body, {
+    const { id: taskID } = req.params
+    const tasks = await TaskModel.findByIdAndUpdate(id, req.body, {
         new: true,
         runValidators: true
     })
-    if (!task) {
+    if (!tasks) {
         return next(createCustomError('no task with the mentioned ID',404))
     }
-    res.status(200).json({ success: true, data: task })
+    res.status(200).json({ tasks })
 
 })
 
